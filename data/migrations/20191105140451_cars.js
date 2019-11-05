7ff4987a8231a8701407b31654b014559dacf95c
CScori <amilanotte1366@gmail.com>
2019-11-05 14:17:10 -0600

    adds table plan

--- data/migrations/20191105140451_cars.js
+++ data/migrations/20191105140451_cars.js
@@ -1,11 +1,25 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('cars', function (table) {
+        table.increments();
+        table.string('name');
+        
+        /*
+        vin integer not null
+        make string not null
+        model string not null
+        miles integer not null
+        trans string 
+        title string
+        */
+        table.timestamps();
+      })
   
 };
 
 
 exports.down = function(knex) {
-    return knex.schema.table('pets', function (table) {
+    return knex.schema.table('cars', function (table) {
         table.dropColumn('name');
         table.string('first_name');
         table.string('last_name');
