5693b3e74e72709ce6b4b7e2cdd10d206413a403
CScori <amilanotte1366@gmail.com>
2019-11-05 14:29:43 -0600

    adds db for 3

--- data/migrations/20191105140451_cars.js
+++ data/migrations/20191105140451_cars.js
@@ -2,22 +2,25 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cars', function (table) {
         table.increments();
-        table.string('name');
-        
-        /*
-        vin integer not null
-        make string not null
-        model string not null
-        miles integer not null
-        trans string 
-        title string
-        */
-        table.timestamps();
+        table.string('car_make', 64).notNullable();
+        table.string('car_model', 64).notNullable();
+        table.float('vin').notNullable();
+        table.float('mileage').notNullable();
+        table.string('transmission_type', 64);
+        table.string('title_status', 64)
+        table.timestamps(true, true);
       })
   
 };
 
-
+ /*
+        vin integer not null
+        make string not null /
+        model string not null/
+        miles integer not null
+        trans string 
+        title string
+        */
 exports.down = function(knex) {
     return knex.schema.table('cars', function (table) {
         table.dropColumn('name');
